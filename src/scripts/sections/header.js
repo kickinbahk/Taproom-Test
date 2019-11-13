4173b0b65cfbc8ec3e3434480a838844bac77a13
kickinbahk <kickinbahk@gmail.com>
2019-11-13 07:11:16 -0800

    add javascript to open menu item when hovered over

--- src/scripts/sections/header.js
+++ src/scripts/sections/header.js
@@ -6,3 +6,13 @@ window.$ = $;
 $('#announcement-bar-close').bind('click touchstart', function(event) {
   $('#announcement-bar').addClass('closed');
 });
+
+$('.mega-menu-parent').on('hover', function(e) {
+  if(e.type == 'mouseenter') {
+    $('.mega-menu').removeClass('closed');
+  }
+
+  if (e.type == 'mouseleave') {
+    $('.mega-menu').addClass('closed');
+  }
+});
