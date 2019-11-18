669c6693762b644fe71fb1e736079dbd2f1f4016
kickinbahk <kickinbahk@gmail.com>
2019-11-17 19:10:15 -0800

    add product handle to template and use jquery call to get product data

--- src/scripts/sections/featured-collection.js
+++ src/scripts/sections/featured-collection.js
@@ -13,5 +13,16 @@ $('.quick-view-button').on('click touchstart', function(e) {
   e.preventDefault();
 
   console.log('button clicked');
+
+  var productTitle = $(this).closest('.featured-collection-item').attr('data-productHandle')
+  var productTitleURLSafe = productTitle.replace(/ /g, '-');
+  var url = '/products/' + productTitleURLSafe +'.js';
+
+  console.log(url)
+
+  $.getJSON(url, function(product) {
+    alert('The title of this product is ' + product.title);
+  } );
+
   $('.modal-overlay').removeClass('closed');
 });
