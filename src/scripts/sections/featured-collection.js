13bc68d4a0ee2d1930876be43afd9b8093e7462f
kickinbahk <kickinbahk@gmail.com>
2019-11-17 16:03:07 -0800

    add overlay for modal

--- src/scripts/sections/featured-collection.js
+++ src/scripts/sections/featured-collection.js
@@ -7,10 +7,11 @@ $('.featured-collection-item').hover(function() {
   $(this).find('.featured-collection__image .quick-view').addClass('show');
 }, function() {
   $(this).find('.featured-collection__image .quick-view').removeClass('show');
-})
+});
 
 $('.quick-view-button').on('click touchstart', function(e) {
   e.preventDefault();
-  
+
   console.log('button clicked');
-})
+  $('.modal-overlay').removeClass('closed');
+});
